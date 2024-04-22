c88132b064d14184112b48352fcb7988345f89e5
Sami Khan <sami999khan999@gmail.com>
2024-04-22 23:15:25 +0600

    modile nav done

---
+++ src/components/SideBar.jsx
@@ -0,0 +1,49 @@
+import React from "react";
+import { FaTasks } from "react-icons/fa";
+import { MdOutlineDashboard } from "react-icons/md";
+import NavLink from "./NavLink";
+
+const sideBarData = [
+  {
+    name: "Dashboard",
+    icon: <MdOutlineDashboard />,
+  },
+  {
+    name: "Dashboard",
+    icon: <MdOutlineDashboard />,
+  },
+  {
+    name: "Dashboard",
+    icon: <MdOutlineDashboard />,
+  },
+  {
+    name: "Dashboard",
+    icon: <MdOutlineDashboard />,
+  },
+  {
+    name: "Dashboard",
+    icon: <MdOutlineDashboard />,
+  },
+  {
+    name: "Dashboard",
+    icon: <MdOutlineDashboard />,
+  },
+];
+
+const SideBar = () => {
+  return (
+    <aside className="bg-blue-200 shadow-2xl w-1/2 h-screen  md:w-1/6 transition-all hidden md:block">
+      <div className="pl-4 flex  flex-col gap-8 mt-4">
+        <div className="flex items-center gap-2">
+          <FaTasks />
+          <p>TastTracker</p>
+        </div>
+        <div className="flex flex-col gap-6">
+          <NavLink />
+        </div>
+      </div>
+    </aside>
+  );
+};
+
+export default SideBar;
--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -1,7 +1,11 @@
 import React from "react";
 
 const Dashboard = () => {
-  return <div>Dashboard</div>;
+  return (
+    <div className="text-primary dark:text-secondary bg-background h-screen">
+      Dashboard
+    </div>
+  );
 };
 
 export default Dashboard;
